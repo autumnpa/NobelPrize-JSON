46b4a625819f1b2c77007269f848be8d54bf3676
Autumn Parker <agmparker@gmail.com>
2023-01-14 16:25:44 -0600

    Updated let loop to include relevant array object to loop through - prizes Updated the parameters that are being looped through from the JSON file - year and category

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@ import data from '../json/nobelprize.json' assert { type: 'json' };
 const output = document.getElementById("output");
 
 let html = "";
-for (let i = 0; i < data.length; i++) {
-    html += `<p>${data[i].name}: ${data[i].value}</p>`;
+for (let i = 0; i < data.prizes.length; i++) {
+    console.log(i);
+    html += `<p>${data.prizes[i].year}: ${data.prizes[i].category}</p>`;
 }
 output.innerHTML = html;
