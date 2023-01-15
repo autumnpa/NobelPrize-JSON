626fb1214d22de14f588f9492daeede0959e783e
Autumn Parker <agmparker@gmail.com>
2023-01-14 19:19:04 -0600

    Updated line that creates a paragraph each time it loops. Now a styled <div> is created each time it loops holding info pulled from JSON file.

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,14 @@ const output = document.getElementById("output");
 let html = "";
 for (let i = 0; i < data.prizes.length; i++) {
     console.log(i);
-    html += `<p>${data.prizes[i].year}: ${data.prizes[i].category}</p>`;
+    // html += `<p>${data.prizes[i].year}: ${data.prizes[i].category}</p>`;
+
+    html += `
+    <div class = "category">
+    <h1>
+    ${data.prizes[i].year} - ${data.prizes[i].category}
+    </h1>
+    </div>
+    `
 }
 output.innerHTML = html;
