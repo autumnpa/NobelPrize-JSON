a7a5cf188f2bd6f6bd0b0b56d4140918898a6855
Autumn Parker <agmparker@gmail.com>
2023-01-16 10:02:24 -0600

    Pulled data out of laureates array. Created loops and if statement to loop through and print object data while skipping any undefined objects.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,44 @@
 import data from '../json/nobelprize.json' assert { type: 'json' };
 const output = document.getElementById("output");
 
-let html = "";
-for (let i = 0; i < data.prizes.length; i++) {
-    console.log(i);
-    // html += `<p>${data.prizes[i].year}: ${data.prizes[i].category}</p>`;
+const laureates = data.prizes[2].laureates;
+console.log(laureates);
 
+let html = "";
+// Maybe change this into a table for better formatting?
+for (let i = 0; i < data.prizes.length; i++) {
+    // html += `<p>${data.prizes[i].year}: ${data.prizes[i].category}</p>`;
+    console.log(data.prizes[i].laureates);
     html += `
     <div class = "category">
     <h1>
-    ${data.prizes[i].year} - ${data.prizes[i].category}
-    </h1>
-    <div class = "laureates">
-    ${data.prizes[i].laureates}
-    </div>
-    </div>
-    `
+    ${data.prizes[i].year} - ${data.prizes[i].category} - `
+
+    if (data.prizes[i].laureates === undefined) {
+        // Object is undefined
+    } else {
+        for (let j = 0; j < data.prizes[i].laureates.length; j++) {
+
+            html += `${data.prizes[i].laureates[j].firstname} - `
+        }
+    }
+
+    html += `
+    </h1 >
+            <div class="laureates">
+            </div>
+    </div >
+            `
 }
+
+// for (let i = 0; i < data.laureates.length; i++) {
+
+//     html += `
+// <div class = "category">
+// <div class = "laureates">
+// ${data.laureates[i].firstname}
+// </div>
+// </div>
+// `
+// }
 output.innerHTML = html;
